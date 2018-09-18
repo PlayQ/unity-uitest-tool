a149234ac3094c23ca225975d8ca4806d36730b8
Dmitryi Ivanilov <divanilov@playq.net>
2018-09-18 15:03:30 +0300

    noticket - Added test classes that utilize ITestRunnerCallbackReceiver interface

---
+++ Assets/Examples/Test1.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace PlayQ.UITestTools
+{
+    public class Test1 : ITestRunnerCallbackReceiver
+    {
+        public void OnBeforeTestsRunEvent()
+        {
+            Debug.LogWarning("Test1: OnBeforeTestsRunEvent()");
+        }
+
+        public void OnAfterTestsRunEvent()
+        {
+            Debug.LogWarning("Test1: OnAfterTestsRunEvent()");
+        }
+    }
+}
---
+++ Assets/Examples/Test3.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace PlayQ.UITestTools
+{
+    public class Test3 : ITestRunnerCallbackReceiver
+    {
+        public void OnBeforeTestsRunEvent()
+        {
+            Debug.LogWarning("Test3: OnBeforeTestsRunEvent()");
+        }
+
+        public void OnAfterTestsRunEvent()
+        {
+            Debug.LogWarning("Test3: OnAfterTestsRunEvent()");
+        }
+    }
+}
